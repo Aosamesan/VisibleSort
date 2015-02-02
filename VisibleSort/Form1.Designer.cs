f3b228a8a5fb20bf58135c3288efa6db0afd119a
Aosamesan <aosamesan@gmail.com>
2015-02-03 02:29:40 +0900

    First Commit

---
+++ VisibleSort/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace VisibleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnOneStep = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblSortName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("NanumGothic", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStop.Location = new System.Drawing.Point(12, 238);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(206, 40);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "■";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("NanumGothic", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(224, 238);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(247, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "▶";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnOneStep
+            // 
+            this.btnOneStep.Font = new System.Drawing.Font("NanumGothic", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnOneStep.Location = new System.Drawing.Point(477, 238);
+            this.btnOneStep.Name = "btnOneStep";
+            this.btnOneStep.Size = new System.Drawing.Size(209, 40);
+            this.btnOneStep.TabIndex = 0;
+            this.btnOneStep.Text = "|▶";
+            this.btnOneStep.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(674, 155);
+            this.panel1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("NanumGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Items.AddRange(new object[] {
+            "Bubble Sort",
+            "Selection Sort"});
+            this.listBox1.Location = new System.Drawing.Point(692, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 213);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("NanumGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSelect.Location = new System.Drawing.Point(692, 239);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(259, 40);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "선택";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(674, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblSortName
+            // 
+            this.lblSortName.AutoSize = true;
+            this.lblSortName.Font = new System.Drawing.Font("NanumGothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblSortName.Location = new System.Drawing.Point(10, 9);
+            this.lblSortName.Name = "lblSortName";
+            this.lblSortName.Size = new System.Drawing.Size(144, 28);
+            this.lblSortName.TabIndex = 4;
+            this.lblSortName.Text = "Bubble Sort";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 290);
+            this.Controls.Add(this.lblSortName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnOneStep);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sorter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnOneStep;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSortName;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ VisibleSort/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+
+namespace VisibleSort
+{
+    public partial class Form1 : Form
+    {
+        int[] array;
+        Rectangle[] panels;
+        Brush[] digitBrushes;
+        int vPadding = 50;
+        int hPadding = 100;
+        Action selectedSort;
+        Thread threadSort;
+        DoubleBuffer buffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PaintFrame(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            Brush brush = new SolidBrush(panel1.BackColor);
+
+            g.Clear(this.BackColor);
+            foreach (Rectangle r in panels)
+            {
+                g.DrawRectangle(new Pen(Brushes.Black), r);
+            }
+        }
+
+        private void PaintArray()
+        {
+            try
+            {
+                Graphics g = buffer.Background;
+                Graphics moto = panel1.CreateGraphics();
+                Pen pen = new Pen(Color.BlueViolet);
+
+                PaintFrame(g);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    g.DrawString(array[i].ToString(), new Font("나눔고딕", 30), digitBrushes[i], panels[i]);
+                }
+
+                moto.DrawImage(buffer.Image, 0, 0);
+                moto.Dispose();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                MessageBox.Show(exc.Source);
+                Console.WriteLine(exc.StackTrace);
+                threadSort.Abort();
+            }
+        }
+
+        private void BubbleSort()
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    SetDigitBrushes(Brushes.Black, 0, i);
+                    SetDigitBrushes(Brushes.Red, new int[] { j, j + 1 });
+                    PaintArray();
+                    Thread.Sleep(250);
+                    if (array[j] > array[j + 1])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                    SetDigitBrushes(Brushes.HotPink, new int[] { j, j + 1 });
+                    PaintArray();
+                    Thread.Sleep(250);
+                }
+                SetDigitBrushes(Brushes.Blue, i);
+                PaintArray();
+            }
+            SetDigitBrushes(Brushes.Blue);
+            PaintArray();
+        }
+
+        private void SelectionSort()
+        {
+            int minIdx = 0;
+            int j;
+            bool isChanged;
+
+            for (int i = 0; i < 9; i++)
+            {
+                minIdx = i;
+                for (j = i + 1; j < 10; j++)
+                {
+                    isChanged = false;
+                    SetDigitBrushes(Brushes.Red, new int[] { minIdx });
+                    PaintArray();
+                    if (array[j] < array[minIdx])
+                    {
+                        minIdx = j;
+                        isChanged = true;
+                    }
+                    SetDigitBrushes(Brushes.Black, i + 1, 9);
+                    SetDigitBrushes(Brushes.Gold, new int[] { minIdx });
+                    if (!isChanged || i != 0)
+                        SetDigitBrushes(Brushes.Pink, new int[] { j });
+                    PaintArray();
+                    Thread.Sleep(500);
+                }
+
+                int tmp = array[minIdx];
+                array[minIdx] = array[i];
+                array[i] = tmp;
+
+                SetDigitBrushes(Brushes.Blue, 0, i);
+                PaintArray();
+            }
+
+
+            SetDigitBrushes(Brushes.Blue);
+            PaintArray();
+        }
+
+        private void MakeArray()
+        {
+            Random r = new Random();
+            array = new int[10];
+            bool[] check = new bool[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                check[i] = false;
+            }
+
+            int j = 0;
+
+            while (j < 10)
+            {
+                int tmp = r.Next(10);
+                if (check[tmp] == false)
+                {
+                    check[tmp] = true;
+                    array[j++] = tmp;
+                }
+            }
+
+            string numbers = "초기 배열 : ";
+
+            for (j = 0; j < 10; j++)
+            {
+                numbers += array[j];
+                if (j != 9)
+                {
+                    numbers += ", ";
+                }
+            }
+
+            textBox1.Text = numbers;
+        }
+
+        private void SetDigitBrushes(Brush brush, int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                digitBrushes[i] = brush;
+            }
+        }
+
+        private void SetDigitBrushes(Brush brush, int start = 0, int end = 9)
+        {
+            if (end >= start && start > -1 && end < 10)
+            {
+                int[] arr = new int[end - start + 1];
+                for (int i = start; i <= end; i++)
+                {
+                    arr[i - start] = i;
+                }
+
+                SetDigitBrushes(brush, arr);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buffer = DoubleBuffer.getInstance(panel1.Width, panel1.Height);
+            digitBrushes = new Brush[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                digitBrushes[i] = Brushes.Black;
+            }
+
+                panels = new Rectangle[10];
+            int partWidth = (panel1.Width - (2 * hPadding)) / 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                panels[i] = new Rectangle(hPadding + i * partWidth, vPadding, partWidth, panel1.Height - (2 * vPadding));
+            }
+
+            listBox1.SelectedIndex = 0;
+            selectedSort = BubbleSort;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(selectedSort);
+
+            if (threadSort == null || threadSort.ThreadState == ThreadState.Stopped || threadSort.ThreadState == ThreadState.Aborted)
+            {
+                MakeArray();
+                threadSort = new Thread(ts);
+                threadSort.Start();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (threadSort != null)
+            {
+                threadSort.Abort();
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            lblSortName.Text = listBox1.Text;
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    selectedSort = BubbleSort;
+                    break;
+                case 1:
+                    selectedSort = SelectionSort;
+                    break;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (threadSort != null)
+                threadSort.Abort();
+        }
+    }
+
+    public class DoubleBuffer
+    {
+        private static DoubleBuffer instance = null;
+        private Graphics background;
+        private Bitmap bitoffScreen;
+
+        private int width;
+        private int height;
+
+        public int Width { get { return width; } }
+        public int Height { get { return height; } }
+
+        public Graphics Background { get { return background; } }
+        public Bitmap Image { get { return bitoffScreen; } }
+
+        private DoubleBuffer(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            bitoffScreen = new Bitmap(this.width, this.height);
+            background = Graphics.FromImage(bitoffScreen);
+        }
+
+        public static DoubleBuffer getInstance(int width, int height)
+        {
+            if (instance == null)
+            {
+                return (instance = new DoubleBuffer(width, height));
+            }
+            return instance;
+        }
+
+        public static DoubleBuffer getInstance()
+        {
+            if (instance != null)
+                return instance;
+            else
+                throw new NullReferenceException();
+        }
+    }
+}
